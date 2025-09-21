6d825f56b93e6f226e2f17623106fecaf5bcd655
Saddam Khan Lavani <saddamkhanlavani@Saddams-MacBook-Air.local>
2025-09-22 01:25:02 +0530

    deploy push

--- src/Components/SideBar2.jsx
+++ src/Components/SideBar2.jsx
@@ -26,7 +26,7 @@ export const SideBar2 = () => {
           <NotificationCard icon="https://saddamkhanlavani.github.io/Juspay-dashboard/assets/icon-activity1.png" title="You have a bug that needs..." time="Just now" />
           <NotificationCard icon="https://saddamkhanlavani.github.io/Juspay-dashboard/assets/icon-activity2.png" title="Released a new version." time="59 minutes ago" />
           <NotificationCard icon="https://saddamkhanlavani.github.io/Juspay-dashboard/assets/icon-activity3.png" title="Submitted a bug." time="12 hours ago" />
-          <NotificationCard icon="/https://saddamkhanlavani.github.io/Juspay-dashboard/assets/icon-activity4.png" title="Modified data in Page X." time="Today, 11.59 AM" />
+          <NotificationCard icon="https://saddamkhanlavani.github.io/Juspay-dashboard/assets/icon-activity4.png" title="Modified data in Page X." time="Today, 11.59 AM" />
           <NotificationCard icon="https://saddamkhanlavani.github.io/Juspay-dashboard/assets/icon-activity5.png" title="Deleted a page in Project X." time="Feb 2, 2023" />
         </ul>
       </div>
@@ -38,7 +38,7 @@ export const SideBar2 = () => {
         <ul className="sidebar-list">
           <ContactsCard icon="https://saddamkhanlavani.github.io/Juspay-dashboard/assets/icon-natali.png" name="Natali Craig" />
           <ContactsCard icon="https://saddamkhanlavani.github.io/Juspay-dashboard/assets/icon-drew.png" name="Drew Cano" />
-          <ContactsCard icon="https://saddamkhanlavani.github.io/Juspay-dashboard/assets/icon-Orlando.png" name="Orlando Diggs" />
+          <ContactsCard icon="https://saddamkhanlavani.github.io/Juspay-dashboard/assets/icon-orlando.png" name="Orlando Diggs" />
           <ContactsCard icon="https://saddamkhanlavani.github.io/Juspay-dashboard/assets/icon-andi.png" name="Andi Lane" />
           <ContactsCard icon="https://saddamkhanlavani.github.io/Juspay-dashboard/assets/icon-kate.png" name="Kate Morrison" />
           <ContactsCard icon="https://saddamkhanlavani.github.io/Juspay-dashboard/assets/icon-koray.png" name="Koray Okumus" />
