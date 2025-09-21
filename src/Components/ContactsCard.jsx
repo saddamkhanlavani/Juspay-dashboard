3c9a61aaffe9b3b97dc43f108e91c4c4b1176429
Saddam Khan Lavani <saddamkhanlavani@Saddams-MacBook-Air.local>
2025-09-22 00:24:10 +0530

    Images fix test deployment

--- src/Components/ContactsCard.jsx
+++ src/Components/ContactsCard.jsx
@@ -3,7 +3,7 @@ import '../scss/components/ContactsCard.scss'
 export const ContactsCard = ({ icon ="/assets/icon-natali.png",name}) => {
 return <div className ="contacts-card">
 <div className="contacts-card-icon">
-<img src={icon} alt="icon" />
+<img src="https://saddamkhanlavani.github.io/Juspay-dashboard/assets/icon-natali.png" alt="icon" />
 </div>
 <p className="contacts-card-title">{name}</p>
 </div>
